46815cfb12d29acb8b2250f18ae2c0415c22c105
MONOGIOUDIS Pantelis <pantelis.monogioudis@nokia.com>
2019-01-19 07:09:02 -0500

    almost final

--- docs/README.md
+++ docs/README.md
@@ -1,7 +1,5 @@
-# Read docs at https://www.gatsbyjs.org/docs
+# Read docs at https://web.njit.edu/cs634-spring-2019
 
-We intend the docs to be read on gatsbyjs.org — you can read them here of course
+We intend the docs to be read on njit.edu course site — you can read them here of course
 :-) just be warned that links won't often work and other things will be missing
 or less than optimal.
-
-Go forth and build cool stuff.
--- www/src/pages/docs/index.js
+++ www/src/pages/docs/index.js
@@ -24,7 +24,10 @@ class IndexRoute extends React.Component {
 
             In this course we will learn from structured and unstructured data using both classical machine learning, in supervised and unsupervised settings, as well as deeper architectures (convolutional neural networks) in supervised settings only. The schedule below highlights the journey. </p>
             <h2>Books</h2>
-
+            <li> <a href="https://www.microsoft.com/en-us/research/uploads/prod/2006/01/Bishop-Pattern-Recognition-and-Machine-Learning-2006.pdf"> Pattern Recognition & Machine Learning, by Bishop, 2006 </a></li>
+            <li><a href="https://github.com/janishar/mit-deep-learning-book-pdf">Deep Learning, by Ian Goodfellow, et.al., 2016. </a></li>
+            <li><a href="https://www.amazon.com/Hands-Machine-Learning-Scikit-Learn-TensorFlow/dp/1491962291/ref=sr_1_3?ie=UTF8&qid=1547836944&sr=8-3&keywords=hands+on+machine+learning+with+scikit-learn+and+tensorflow">Hands-On Machine Learning with Scikit-Learn and TensorFlow, by Aurélien Géron, 2017</a></li>
+  
             <h2>Class Schedule</h2>
             <p>The schedule is based on <a> https://www.njit.edu/registrar/spring-2019-academic-calendar/ </a> :</p>
             <ol>
@@ -41,13 +44,43 @@ class IndexRoute extends React.Component {
               <strong>Week 4: (2/14/2019) </strong> We review least squares problems and its relationship to Maximum Likelihood and apply the method to problematic situations that reveal overfitting. We understand regularization and how can solve the overfitting problem using a popular house price prediction dataset.  
               </li>
               <li>
-              <strong>Week 5: 2/21/2019</strong> This week we address one of the cornerstones of learning algorithms - the Perceptron and generalized linear models for classification - the Logistic Regression. We review Stochastic Gradient Descent and make the connection between perceptrons and neurons. We expand into shallow neural networks and we use Tensorflow to model neural networks. 
+              <strong>Week 5: (2/21/2019)</strong> This week we address one of the cornerstones of learning algorithms - the Perceptron and generalized linear models for classification - the Logistic Regression. We review Stochastic Gradient Descent and make the connection between perceptrons and neurons. We expand into shallow neural networks and we use Tensorflow to model neural networks. 
               </li>
               <li>
-              <strong>Week 6: 2/28/2019</strong> Kernel methods are used extensively even in today's deep learning dominated field.  They are used as components together with deep neural networks or perhaps more importantly as stand alone solutions to applications that require very small latencies such as robotics. Here we focus on Max Margin Classifiers such as Support Vector Machines. 
+              <strong>Week 6: (2/28/2019) </strong> Kernel methods are used extensively even in today's deep learning dominated field.  They are used as components together with deep neural networks or perhaps more importantly as stand alone solutions to applications that require very small latencies such as robotics. Here we focus on Max Margin Classifiers such as Support Vector Machines. 
               </li>
               <li>
-              <strong>Week 6: 2/28/2019</strong> Kernel methods are used extensively even in today's deep learning dominated field.  They are used as components together with deep neural networks or perhaps more importantly as stand alone solutions to applications that require very small latencies such as robotics. Here we focus on Max Margin Classifiers such as Support Vector Machines. 
+              <strong>Week 7: (3/7/2019) </strong>  The setting this week changes to that of unsupervised learning. We review workhorses of ML such as K-means, KNN and Decision Trees. 
+              </li>
+              <li>
+              <strong>Week 8: (3/14/2019) </strong>  In this week we go over problem statements and the way to go about providing solutions to the midterm projects. Mid-term projects are application / competition oriented and require programming. 
+              </li>
+              <li>
+              <strong>Week 9: (3/21/2019) </strong>  Enjoy your Spring Break!
+              </li>
+              <li>
+              <strong>Week 10: (3/28/2019) </strong>  We start the 2nd half by examining Decision Trees - another ML workhorse with wide applicability in a diverse set of problems (classification and regression). 
+              </li>
+              <li>
+              <strong>Week 11: (4/4/2019</strong>  We conclude the classical machine learning part of the course by looking at probabilistic reasoning. Starting from the Bayesian setting, we introduce Markov Random Fields and Conditional Random Fields. 
+              </li>
+              <li>
+              <strong>Week 12: (4/11/2019) </strong>  We connect back to the perceptron algorithm and we start the exploration into the deep learning space by connecitng multiple perceptrons together in layers. We explain how MLPs learn via SGD and how backpropagation in MLPs dramatically improves the estimation of the required gradients. 
+              </li>
+              <li>
+              <strong>Week 13: (4/18/2019) </strong>  We continue on backpropation by highlighting how we need to control  gradient flow to avoid vanishing gradients or gradient explosion in Deep Neural Networks. We go over a DNN example highlighting the training difficulties / issues. 
+              </li>
+              <li>
+              <strong>Week 14: (4/25/2019) </strong>  CNNs are almost synonymous with deep learning. This architecture although known for some time, it is still finding application in a diverse set of use cases involving unstructured data such as computer vision. We review the principles of convolution and pooling and understand what they offer relative to fully connected layers. At the end we go over a classic object detection example.  
+              </li>
+              <li>
+              <strong>Week 15: (5/2/2019) </strong>  In this week we go over the problem statements of the final projects and the tools that are required for their solution. Final projects are application / competition oriented and require programming. 
+              </li>
+              <li>
+              <strong>Week 16: (5/9/2019) </strong>  We conclude the course with going over a complete end to end tensorflow pipeline use case, spending some time on hyperparameter optimization. 
+              </li>
+              <li>
+              <strong>Week 13: (5/16/2019) </strong>  In this last lecture, we review what we learned and we recognize students to excelled in their final projects by giving them the opportunity to present them.  
               </li>
             </ol>
             {/* <EmailCaptureForm signupMessage="Want to keep up with the latest tips &amp; tricks? Subscribe to our newsletter!" /> */}
